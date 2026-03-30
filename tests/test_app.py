ec5176dcd30ec29ec646eadcbc3383ec8ddff516
2024ht66529-Abhijat <2024ht66529@wilp.bits-pilani.ac.in>
2026-03-30 18:54:04 +0000

    Unit Test updated

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,16 +1,22 @@
-from app import app
+from app import create_app
 
 def test_health_check():
+    app = create_app()
+    app.testing = True
     client = app.test_client()
     response = client.get('/')
     assert response.status_code == 200
 
 def test_calories_success():
+    app = create_app()
+    app.testing = True
     client = app.test_client()
     response = client.get('/calories/70/FL')
     assert response.json["calories"] == 1540
 
 def test_calories_invalid_program():
+    app = create_app()
+    app.testing = True
     client = app.test_client()
     response = client.get('/calories/70/XYZ')
     assert response.status_code == 400
